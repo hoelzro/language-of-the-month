49337aee5c5a1bd6c8e389a328116a5d06f06c52
Rob Hoelz <rob@hoelz.ro>
2016-03-04 08:51:34 -0600

    Elm Typing Tutor: Add work in progress Card module
    This module will contain the logic for creating, rendering,
    and updating "cards", which will display an ngram and monitor
    a user's keypresses for progress.  Right now I'm still figuring
    out the right way to insulate this into a standalone module and keep other
    parts of the application (eg. detecting when the next card should
    be shown, creating new cards) in other modules, so I'm working towards a
    proof of concept that works, which I'll clean up later.  So right now
    this module is a dumping ground for duplicate code, random and
    miscellaneous utility functions

---
+++ Feb-2016/Card.elm
@@ -0,0 +1,157 @@
+import Char
+import Dict
+import Graphics.Element exposing (Element, show)
+import Html exposing (Html, div, hr, text)
+import Html.Attributes exposing (style)
+import Keyboard
+import Random
+import Signal
+import String
+import Time exposing (Time)
+
+import Signal.Extra exposing (foldp')
+
+import RussianNGrams exposing (ngrams)
+
+pairListToDict : List (comparable, v) -> Dict.Dict comparable v
+pairListToDict list =
+  case list of
+    []               -> Dict.empty
+    ((k, v) :: tail) -> Dict.insert k v <| pairListToDict tail
+
+dictToFunction : v -> Dict.Dict comparable v -> comparable -> v
+dictToFunction bottom mapping = \key ->
+  case Dict.get key mapping of
+    Just value -> value
+    Nothing    -> bottom
+
+defineKeyMap : List (Char, Char) -> Char.KeyCode -> Char
+defineKeyMap mapping =
+  let (leftKeys, rightKeys) = List.unzip mapping
+      keyCodes              = List.map Char.toCode leftKeys
+      codesToRightKeys      = List.map2 (\code right -> (code, right)) keyCodes rightKeys
+  in dictToFunction ' ' <| pairListToDict codesToRightKeys
+
+-- https://en.wikipedia.org/wiki/QWERTY
+-- https://en.wikipedia.org/wiki/JCUKEN
+qwerty2jcuken = defineKeyMap [
+  ('q', 'й'), ('w', 'ц'), ('e', 'у'), ('r', 'к'), ('t', 'е'), ('y', 'н'), ('u', 'г'), ('i', 'ш'), ('o', 'щ'), ('p', 'з'),
+  ('[', 'х'), (']', 'ъ'), ('a', 'ф'), ('s', 'ы'), ('d', 'в'), ('f', 'а'), ('g', 'п'), ('h', 'р'), ('j', 'о'), ('k', 'л'), ('l', 'д'), (';', 'ж'), ('\'', 'э'),
+  ('z', 'я'), ('x', 'ч'), ('c', 'с'), ('v', 'м'), ('b', 'и'), ('n', 'т'), ('m', 'ь'), (',', 'б'), ('.', 'ю'), ('/', '.'),
+
+  ('Q', 'Й'), ('W', 'Ц'), ('E', 'У'), ('R', 'К'), ('T', 'Е'), ('Y', 'Н'), ('U', 'Г'), ('I', 'Ш'), ('O', 'Щ'), ('P', 'З'), ('{', 'Х'), ('}', 'Ъ'),
+  ('A', 'Ф'), ('S', 'Ы'), ('D', 'В'), ('F', 'А'), ('G', 'П'), ('H', 'Р'), ('J', 'О'), ('K', 'Л'), ('L', 'Д'), (':', 'Ж'), ('"', 'Э'),
+  ('Z', 'Я'), ('X', 'Ч'), ('C', 'С'), ('V', 'М'), ('B', 'И'), ('N', 'Т'), ('M', 'Ь'), ('<', 'Б'), ('>', 'Ю'), ('?', ',') ]
+
+type Card = Card String String
+
+showCard : Card -> Html
+showCard (Card target typed) =
+  let targetDisplay = text target
+      ruleDisplay = hr [] []
+      typedDisplay = text typed
+      styleAttr =
+    style [("width", "400px"),
+           ("height", "400px"),
+           ("border", "thin solid lightgray"),
+           ("box-shadow", "5px 5px lightgray"),
+           ("position", "absolute"),
+           ("top", "100px"),
+           ("left", "100px")]
+  in div [styleAttr] [ targetDisplay, ruleDisplay, typedDisplay ]
+
+updateCard : Char -> Card -> Card
+updateCard character (Card target typed) =
+  let newTyped = typed ++ (String.fromChar character)
+  in Card target newTyped
+
+{-
+handleCard : Signal Char -> Card -> Signal Html
+handleCard keyPresses card =
+  Signal.map showCard <| Signal.foldp updateCard card keyPresses
+
+main : Signal Html
+main = Signal.map showCard <| Signal.foldp updateCard (Card "вать" "") <| Signal.map qwerty2jcuken Keyboard.presses
+-}
+
+type Event = Clock Time
+  | Keypress Char
+
+type alias State = {
+  currentCard : Card,
+  seed : Random.Seed
+}
+
+makeCard : String -> Card
+makeCard s = Card s ""
+
+-- XXX unsafeFromJust
+fromJust : Maybe a -> a
+fromJust maybeValue =
+  case maybeValue of
+    Just value -> value
+    Nothing    -> Debug.crash "Got Nothing when Just was expected"
+
+last : List a -> a
+last = fromJust << List.head << List.reverse
+
+selectValue : List (Float, a) -> Float -> a
+selectValue pairs selector =
+  let lowerThanSelector = List.filter (\(p, _) -> p <= selector) pairs
+  in snd <| last lowerThanSelector
+
+weightedChoice : List (Float, a) -> Random.Generator a
+weightedChoice possibilities =
+  let (proportions, values) = List.unzip possibilities
+      total = List.sum proportions
+      cumulativeProportions = List.scanl (+) 0 proportions
+      cumulativePossibilties = List.map2 (\a b -> (a, b)) cumulativeProportions values
+      generateProportionValue = Random.float 0.0 (total - 1)
+  in Random.map (selectValue cumulativePossibilties) generateProportionValue
+
+unsafeIndex : List a -> Int -> a
+unsafeIndex values index =
+  case (index, values) of
+    (_, [])           -> Debug.crash "index too large"
+    (0, (value :: _)) -> value
+    (_, (_ :: tail))  -> unsafeIndex tail <| index - 1
+
+randomMember : List a -> Random.Generator a
+randomMember values =
+  let numValues = List.length values
+      indexGenerator = Random.int 0 (numValues - 1)
+  in Random.map (unsafeIndex values) indexGenerator
+
+randomNgram : List (Int, List (Float, String)) -> Random.Generator String
+randomNgram ngrams =
+  let randomNgramSet = randomMember <| List.map snd ngrams
+  in randomNgramSet `Random.andThen` weightedChoice
+
+generateRandomCard : State -> (State, Card)
+generateRandomCard state =
+  let (ngram, seed1) = Random.generate (randomNgram ngrams) state.seed
+  in ({state | seed = seed1}, makeCard ngram)
+
+view : State -> Element
+view state =
+  show state
+
+update : Event -> State -> State
+update event state =
+  let _ = Debug.log "event:" event
+  in case event of
+    Clock t    ->
+      let tempState        = { state | seed = Random.initialSeed <| round t }
+          (newState, card) = generateRandomCard tempState
+      in { newState | currentCard = card }
+    Keypress c -> state -- XXX for now
+
+main : Signal Element
+main =
+  let startingTimestamp = Signal.map (Clock << fst) <| Time.timestamp <| Signal.constant ()
+      inputChars = Signal.map (Keypress << qwerty2jcuken) Keyboard.presses
+      combined = Signal.mergeMany [startingTimestamp, inputChars]
+
+      initialState = { currentCard = Card "" "", seed = Random.initialSeed 0 }
+
+  in Signal.map view <| foldp' update (\event -> update event initialState) combined
